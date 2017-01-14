263093250e04b35c68aa5bd6972940f41fb7cdf7
Camilo Riviere <SameThingButCheaper@SameThingButCheaper.com>
2017-01-14 13:44:34 -0500

    Updated settings, created Procfile and requirements so we can deploy to Heroku

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -114,8 +114,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIR = ( BASE_DIR + '/www/static',)
+
+# Extra places for collectstatic to find static files.
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
